262ed45b26cd106e2789d8e7b58807c68a0a0768
Wellington Alan <wellingtonalan@Wellingtons-MacBook-Air.local>
2014-04-08 15:38:24 -0300

    Criando HospedaOn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hospedaon::Application.config.secret_key_base = '126712cb20b42dcea7cc49609784ffeb4723b4d2c0f07a9b33bb76b5062c15889c01f3cef961de22192429767206cad252f52296ab767a3f572b36e4cc3fabcb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hospedaon::Application.config.session_store :cookie_store, key: '_hospedaon_session'
